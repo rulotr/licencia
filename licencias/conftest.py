e37d0eebecce367fd1daed1c6d8644e666f56e60
raultr <RAUL.TORRES@cusaem.gob.mx>
2021-05-05 20:34:08 -0500

    Commit inicial

---
+++ licencias/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from licencias.users.models import User
+from licencias.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
